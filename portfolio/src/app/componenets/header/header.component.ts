74598f735a790738b1b9164cb779e7ba8e128949
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-12-26 20:49:28 -0800

    Feature: Header
    - Created header
    - Used name for homepage link
    - Created links for Project and Education sections
    - Added icons for light and dark mode.
    - Functionality of header will be implemented further as the other sections are added.

--- portfolio/src/app/componenets/header/header.component.ts
+++ portfolio/src/app/componenets/header/header.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  lightMode: boolean;
 
+  constructor() {
+    this.lightMode = true;
+  }
+
+  onModeClick(bool: boolean) {
+    this.lightMode = bool;
+    if(this.lightMode) {
+      console.log("Light mode is on");
+    } else {
+      console.log("Dark mode is on");
+    }
+  }
 }
