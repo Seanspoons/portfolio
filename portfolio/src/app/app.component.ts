a7163286bb42a3a2151811c4af812f3174bacc5c
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2023-12-26 22:28:59 -0800

    Feature: Info Page
    - Created info page
    - Added title at top
    - Created profile picture
    - Implemented GitHub and LinkedIn links
    - Need to add resume to files and get email working.
    - Sizing needs work as current look is unknown on a larger screen size

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componenets/header/header.component';
+import { InfoComponent } from './componenets/info/info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule,
+      RouterOutlet,
+      HeaderComponent,
+      InfoComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- portfolio/src/app/componenets/info/info.component.ts
+++ portfolio/src/app/componenets/info/info.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class InfoComponent {
 
+  onResumeClick(): void {
+    /*const resumeURL = '';*/
+  }
+
+  onGitHubClick(): void {
+    const gitHubURL = 'https://github.com/Seanspoons';
+    window.open(gitHubURL, '_blank');
+  }
+
+  onLinkedInClick(): void {
+    const linkedInURL = 'https://www.linkedin.com/in/sean-wotherspoon-a085122a4/';
+    window.open(linkedInURL, '_blank');
+  }
+
+  onEmailClick(): void {
+    /*const emailURL = '';*/
+  }
+
 }
